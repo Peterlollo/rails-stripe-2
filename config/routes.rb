f794d994f7394c07e641dd0a0c8d419d984404e8
Peter Lollo <peteralollo@gmail.com>
2016-06-07 11:47:25 -0700

    Write payment and user rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root to: 'application#angular'
 
+  resources :users, only: [:create, :index, :show] do
+    resources :payments, only: [:show, :create] do
+      member do
+        post '/sendPayment' => 'payments#token'
+      end
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
